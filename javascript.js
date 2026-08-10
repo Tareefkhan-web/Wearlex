0db86c22fc584c3ffc23e38c4a89338c6be12d15
Tareef Khan <ktareef096l@gmail.com>
2026-08-10 13:44:17 -0700

    Initial commit for Wearlex website

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// loader
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    loader.style.opactiy = "0";
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 500);
+});
+
+//responsivnas of ipad
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    })
+}
+
+if (close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
